c248cb16e226a9e33fb22c3cda8ffa917953dabe
kabil <128507560+kabilg143@users.noreply.github.com>
2023-03-21 22:27:36 +0530

    Add files via upload

---
+++ string inbuildfun.php
@@ -0,0 +1,75 @@
+<html>
+    <body bgcolor="aqua">
+<h1 align="center">string  inbult functions</h1>
+<?php
+
+ $txt="kabil";
+
+ echo "name:"."$txt <br>";
+
+ //echo "length:".strlen($txt);
+ echo "<br>";
+
+ //echo "uppercase:".strtoupper($txt) ;
+ echo "<br>";
+
+ //echo "lowercase:".strtolower($txt);
+ //echo "<br>";
+
+ //echo "words:".str_word_count($txt);
+ //echo "<br>";
+
+ //echo "reverse:".strrev($txt);
+ //echo "<br>";
+
+ //echo "position:".strpos($txt,"l");
+ //echo "<br>";
+
+//echo "new name:".str_replace("k","g",$txt);
+//echo "<br>";
+
+
+//echo addcslashes($txt,"a");
+//echo "<br>";
+
+//echo addslashes("hi 'hello wecome");
+
+//echo bin2hex($txt);
+
+//echo chop("hi hello","hello");
+
+//echo chr(65);
+
+//echo chunk_split($txt,1,".");
+
+/*$ec= convert_uuencode($txt);
+echo $ec;
+$dc=convert_uudecode($ec);
+echo $dc;*/
+
+//echo count_chars("kabiglibak",3);
+
+//$str="a,b,c";
+//print_r(explode(",",$str));
+
+//$str=array("hi","hello");
+//echo implode(",",$str);
+//echo join("+",$str);
+
+//echo hex2bin("48656c6c6f20576f726c6421");
+
+//echo lcfirst("KABIL");
+//echo ucfirst("kabil");
+//echo ucfirst("hello world");
+
+//$str="hello world";
+//echo ltrim($str,"hello");
+
+//echo rtrim($str,"world");
+
+//echo trim($str);
+
+
+?>
+</body>
+</html>
